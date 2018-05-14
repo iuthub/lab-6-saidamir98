720a036b68dd83de57b51b9991c393875e57ed8a
Saidamir Botirov <saidamir.botirov@gmail.com>
2018-05-14 18:45:36 +0500

    by U1610263

---
+++ mymdb.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kevin Bacon</title>
+	<link rel="stylesheet" href="bacon.css">
+</head>
+<body>
+	<div class="input-form">
+		<form action="post">
+			Actor's first/last name: <input type="text" name="first_name"> <input type="text" name="last_name"> <button type="button">GO</button>
+		</form>
+	</div>
+
+	<h1>The One Degree of Kevin Bacon</h1>	
+	<p>Type in an actor's name to see if he/she was ever in a movie with Kevin Bacon!</p>
+	<img src="res/Kevin.jpg" alt="Kevin">
+</body>
+</html>
